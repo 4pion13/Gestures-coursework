11698eb0b30191b1f236dfdeb48d18b2e2ce7f31
Роман <i4pion@yandex.ru>
2024-09-24 15:28:03 +0300

    Добавлена отправка файлов запросом на сервер и возврат пути для отображения видео

--- components/Buttons/FileUploadBtn.js
+++ components/Buttons/FileUploadBtn.js
@@ -7,11 +7,12 @@ class FileUploadBtn {
         `;
         this.loadingBtn = `
             <div class="align-items-end p-2" style = "position:absolute; bottom:0; width:100%;">
-                <button class="btn btn-primary" type="button" disabled>
+                <button class="btn bg-secondary-subtle col-12" type="button" disabled>
                     <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
                 </button>
             </div> 
-        `
+        `;
+        this.dataBtn = '';
 
         this.loadingStatus = 0;
 
@@ -19,25 +20,52 @@ class FileUploadBtn {
     }
 
 
-
+    updateBtn(){
+        if (this.loadingStatus === 1) {
+            this.dataBtn = this.loadingBtn;
+        }else{
+            this.dataBtn = this.fileUploadBtnIn;
+        }
+        dialogue.render();
+    }
 
     sendingFile(input){
         //const fileInput = document.getElementById('formFile');
         let file = input.files[0];
-        alert(`File name: ${file.name}`); // например, my.png
-        alert(`Last modified: ${file.lastModified}`);
         const formData = new FormData();
         formData.append('file', file);
         this.loadingStatus = 1;
-        if (this.loadingStatus === 1) {
-            this.fileUploadBtnIn = this.loadingBtn;
-        }
-        console.log(this.loadingStatus);
-        dialogue.render();
+        this.updateBtn();
+        fetch('http://localhost:8000/upload/', {
+            method: 'POST',
+            body: formData,
+            //headers: {
+                // Вы можете добавить заголовки, если это необходимо
+                // 'Authorization': 'Bearer your_token',
+            //}
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            //console.log(response.json());
+            return response.json();
+        })
+        .then(data => {
+            console.log('Success:', data.file_url);
+            var me = this;
+            setTimeout(function(){
+                me.loadingStatus = 0;
+                me.updateBtn();
+                dialogue.render();
+            }, 1000);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+            this.loadingStatus = 0;
+            dialogue.render();
+        });
         
-
-
-
     }
 
     fileUploadBtnInner(){
@@ -47,14 +75,7 @@ class FileUploadBtn {
         } else {
             html += this.loadingBtn;
         }
-        console.log(html);
-
-        return html;
-
-
-    
-
-    
+        return html;    
     }
 
 }
--- components/Dialogue/Dialogue.js
+++ components/Dialogue/Dialogue.js
@@ -5,12 +5,12 @@ class Dialogue{
         <div>
             <div class="d-flex justify-content-center">
                 <h2>Диалог</h2>
+                <video src="http://localhost:8000/media/videos/f17a6060-6ced-4bd1-9886-8578cfbb864f_1.mp4" style="width: 300px;" controls></video>
             </div>
         </div>
         
         `
         let htmlBtn = fileUploadBtn.fileUploadBtnInner();
-
         const html = `
             <div class = "w-100 h-100 bg-secondary-subtle chat-history shadow" style = "position:relative;">
                 ${htmlDialogue}
